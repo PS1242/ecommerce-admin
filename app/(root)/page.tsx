66fc3be929c130e81c4e31d0b515452b7e96d39a
Prashant <prashantsoni1242@gmail.com>
2023-09-03 18:32:00 +0530

    init

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <div className="p-2">
+        <Button variant={"secondary"}>Click Me</Button>
+      </div>
+    </>
+  )
+}
