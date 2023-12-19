8f3aa60fb6dc16da001ff5860dc2df68bf17217b
Prashant <prashantsoni1242@gmail.com>
2023-12-19 10:49:30 +0530

    creates root layout file for auth pages

---
+++ app/(root)/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function authPage({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex items-center justify-center h-full">{children}</div>
+  );
+}
