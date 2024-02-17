fb7f3d85f457b26c2d7d81b24326ef57e369b350
Prashant <prashantsoni1242@gmail.com>
2024-02-17 17:19:17 +0530

    billboard creation done

--- app/(dashboard)/[storeId]/(routes)/billboards/components/BillBoardClient.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/BillBoardClient.tsx
@@ -4,8 +4,11 @@ import { Button } from "@/components/ui/button";
 import Heading from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { PlusIcon } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
 
 export default function BillBoardClient() {
+  const params = useParams();
+  const router = useRouter();
   return (
     <>
       <div className="flex items-center justify-between">
@@ -13,7 +16,9 @@ export default function BillBoardClient() {
           title="Billboards(0)"
           description="Manage billboard for your store"
         />
-        <Button>
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
           <PlusIcon className="mr-2 h-4 w-4" />
           Add new
         </Button>
