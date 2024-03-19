228f2a6c99b890f566d8ee942d7c5baec790d449
Prashant <prashantsoni1242@gmail.com>
2024-03-19 21:35:15 +0530

    fix: removes unnecessary separator

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/BillBoardForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/BillBoardForm.tsx
@@ -166,7 +166,6 @@ export default function BillBoardForm(props: BillBoardFormProps) {
             {action}
           </Button>
         </form>
-        <Separator />
       </Form>
     </>
   );
